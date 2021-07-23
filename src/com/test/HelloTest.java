5c61fff8b51b7fea5fdaebe6dab4b65cfc3a730f
zhoujw1974 <84754784@qq.com>
2021-07-23 16:10:49 +0800

    second commit

---
+++ src/com/test/HelloTest.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class HelloTest {
+
+public static void main(String args[])
+{
+    System.out.println("Hello, world !!!");
+    MyDog myDog = new MyDog();
+    myDog.setName("My Dog Love");
+    System.out.println(myDog.getName());
+}
+}
---
+++ src/com/test/MyDog.java
@@ -0,0 +1,28 @@
+package com.test;
+
+public class MyDog {
+
+ private String name= "my love";
+ private int  age;
+
+ public String getName()
+ {
+     return this.name ;
+
+ }
+
+ public String setName(String  name)
+    {
+        return this.name = name ;
+
+    }
+
+ public int  getAge()
+ {
+     return this.age;
+ }
+    public int  setAge(int age)
+    {
+        return this.age = age;
+    }
+}
